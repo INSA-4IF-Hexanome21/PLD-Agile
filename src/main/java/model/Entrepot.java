e89d4bf2134b9b8a448cc6397496333bd956dd18
GarouxE <ewan.garoux44@gmail.com>
2025-10-13 11:08:01 +0200

    mise a jour de Site, Delivery, Pickup et entrepot

--- src/main/java/model/Entrepot.java
+++ src/main/java/model/Entrepot.java
@@ -1,8 +1,14 @@
 package model;
 
 public class Entrepot extends Site {
+    
+    // Constructeur minimal (lat/lng null)
+    public Entrepot(long id, Integer numLivraison, Integer dureeRecup) {
+        super(id); // lat/lng null
+    }
 
-    public Entrepot(long id, float lat, float lng) {
+    // Constructeur complet (lat/lng fournis)
+    public Entrepot(long id, Float lat, Float lng) {
         super(id, lat, lng);
     }
 
