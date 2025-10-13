186bdb2ed5acd4c8fc37c789d898feba04aac376
GarouxE <133769796+GarouxE@users.noreply.github.com>
2025-10-13 11:25:23 +0200

    fix: mettre à jour Site, Delivery, Pickup et Entrepot

--- src/main/java/model/Pickup.java
+++ src/main/java/model/Pickup.java
@@ -2,13 +2,38 @@ package model;
 
 public class Pickup extends Site {
 
+    //Attributs
     protected Integer numLivraison;
     protected Integer dureeRecup;
 
-    public Pickup(long id, float lat, float lng, Integer numLivraison, Integer dureeRecup) {
+    // Constructeur minimal (lat/lng null)
+    public Pickup(long id, Integer numLivraison, Integer dureeRecup) {
+        super(id); // lat/lng null
+        this.numLivraison = numLivraison;
+        this.dureeRecup = dureeRecup;
+    }
+
+    // Constructeur complet (lat/lng fournis)
+    public Pickup(long id, Float lat, Float lng, Integer numLivraison, Integer dureeRecup) {
         super(id, lat, lng);
         this.numLivraison = numLivraison;
         this.dureeRecup = dureeRecup;
     }
 
+    // Getters / Setters 
+    public Integer getNumLivraison() {
+        return numLivraison;
+    }
+
+    public void setNumLivraison(Integer numLivraison) {
+        this.numLivraison = numLivraison;
+    }
+
+    public Integer getDureeRecup() {
+        return dureeRecup;
+    }
+
+    public void setDureeRecup(Integer dureeRecup) {
+        this.dureeRecup = dureeRecup;
+    }
 }
