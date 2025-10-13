c3acbf75665242b6d6125bcb45892be8063e1afa
naknin <aknin.nathan5@gmail.com>
2025-10-13 09:02:19 +0200

    Refactor CarteTest to use Livreur instance in Trajet initialization

--- src/test/java/model/CarteTest.java
+++ src/test/java/model/CarteTest.java
@@ -5,7 +5,8 @@ public class CarteTest {
     public static void main(String[] args) {
          System.out.println("===== TEST Carte =====");
         Carte carte =new Carte();
-        Trajet trajet = new Trajet(1);
+        Livreur livreur = new Livreur(1, "John", "Doe");
+        Trajet trajet = new Trajet(livreur);
         
         carte.ajouterTrajet(trajet);
         System.out.println("Nombre de trajets dans la carte : " + carte.getTrajets().size());
