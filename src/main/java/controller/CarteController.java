aeb3b219ce46353b2807f9e8628ac1010be6ec67
Juan Jose Diaz Ortega <123647043+jjdiazo1@users.noreply.github.com>
2025-10-13 04:25:26 -0500

    Initialization de la carte (#16)

--- src/main/java/controller/CarteController.java
+++ src/main/java/controller/CarteController.java
@@ -2,6 +2,8 @@ package controller;
 
 import model.Carte;
 import model.GestionnaireXML;
+import model.Noeud;
+import model.Troncon;
 
 public class CarteController {
     private Carte carte;
@@ -20,6 +22,27 @@ public class CarteController {
             carte.ajouterTroncon(t);
         }
     }
+
+    public String getCarteJSON() {
+    StringBuilder json = new StringBuilder();
+    json.append("{\"noeuds\":[");
+    for (int i = 0; i < carte.getNoeuds().size(); i++) {
+        Noeud n = carte.getNoeuds().get(i);
+        json.append(String.format("{\"id\":%d,\"lat\":%f,\"lng\":%f}",
+                n.getId(), n.getLatitude(), n.getLongitude()));
+        if (i < carte.getNoeuds().size() - 1) json.append(",");
+    }
+    json.append("],\"troncons\":[");
+    for (int i = 0; i < carte.getTroncons().size(); i++) {
+        Troncon t = carte.getTroncons().get(i);
+        json.append(String.format("{\"from\":%d,\"to\":%d}",
+                t.getOrigine().getId(), t.getDestination().getId()));
+        if (i < carte.getTroncons().size() - 1) json.append(",");
+    }
+    json.append("]}");
+    return json.toString();
+    }
+
 
     public Carte getCarte() {
         return carte;
