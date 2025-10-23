807b03c609bc08724475791fd45d08c96577175c
elie.ravoux <elie.ravoux@gmail.com>
2025-10-23 11:21:50 +0200

    chore : Ajout de commentaires

--- src/main/java/model/DemandeLivraison.java
+++ src/main/java/model/DemandeLivraison.java
@@ -3,16 +3,16 @@ package model;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.AbstractMap.SimpleEntry;
 
+//Classe permettant de stocker une demande de livraison
 public class DemandeLivraison {
 
     // Attributs
     private Entrepot entrepot;
     private HashMap<Long,Site> sites;
-    private HashMap<Integer,List<Long>> livraisons;
-    private HashMap<Integer,Boolean> livraisonsAssignees;
-    private Integer nbLivraisonsNonAssignees;
+    private HashMap<Integer,List<Long>> livraisons; //Hashmap associant le numéro d'une livraisons aux id des sites de collectes et de dépots
+    private HashMap<Integer,Boolean> livraisonsAssignees; // Hashmap permettant de savoir si une livraison est assigné à partir de son numéro de livraison
+    private Integer nbLivraisonsNonAssignees; 
 
 
     // Constructeur complet
@@ -29,13 +29,23 @@ public class DemandeLivraison {
         return sites.get(idSite);
     }
 
+     public List<Site> getSites(){
+        return new ArrayList<Site>(sites.values());
+    }
+
+    //Fonction permettant d'assigner un livreur à une livraison
+    //Elle renvoie le nombre de livraisons qu'il reste à assigner
     public Integer assignerLivreur(Livreur livreur, Integer idLivraison, Carte carte ){
-        Trajet trajet = carte.getTrajetParLivreur(livreur.getId());
+        
         List<Long> idsSiteslivraison = livraisons.get(idLivraison);
+        //On vérifie si la livraison a déjà été assignée si ce n'est pas le cas on la défini comme assignée
         if(!livraisonsAssignees.get(idLivraison)){
             livraisonsAssignees.put(idLivraison,true);
             --this.nbLivraisonsNonAssignees;
         }
+        
+        Trajet trajet = carte.getTrajetParLivreur(livreur.getId());
+        
         if(trajet == null){
             trajet = new Trajet(livreur);
             trajet.addSite(entrepot);
@@ -44,12 +54,15 @@ public class DemandeLivraison {
         else{
             trajet.setLivreur(livreur);
         }
+
+        //On ajoute les sites associés à la livraison dans le trajet associé au livreur
         for(Long id : idsSiteslivraison){
             trajet.addSite(sites.get(id));
         }
         return this.nbLivraisonsNonAssignees;
     }
 
+    //Permet d'ajouter un site
     public void addSite(Site site, boolean estEntrepot){
         Long idSite = site.getId();
         if(estEntrepot){
@@ -70,17 +83,12 @@ public class DemandeLivraison {
                 List<Long> idsSites = new ArrayList<Long>();
                 idsSites.add(idSite);
                 livraisons.put(numLivraison,idsSites);
+
+                //Il s'agit d'une nouvelle livraison donc on la défini comme non assignée
                 livraisonsAssignees.put(numLivraison,false);
                 ++nbLivraisonsNonAssignees;
             }
         }
         sites.put(idSite,site);
-    }
-
-    public List<Site> getSites(){
-        return new ArrayList<Site>(sites.values());
-    }
-
-
-    
+    }    
 }
