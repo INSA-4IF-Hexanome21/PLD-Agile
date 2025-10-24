354b843be391cef09bc6ba88998fc26b03c568e0
Peter Yaacoub <yaapete.dev@gmail.com>
2025-10-24 13:22:47 +0000

    chore: retirer l'itérateur commenté

--- src/main/java/tsp/IteratorSeq.java
+++ src/main/java/tsp/IteratorSeq.java
@@ -11,23 +11,6 @@ public class IteratorSeq implements Iterator<Integer> {
 	private Integer[] candidats;
 	private int nbCandidats;
 
-	/**
-	 * Cree un iterateur pour iterer sur l'ensemble des sommets de nonVus qui sont successeurs de sommetCrt dans le graphe g,
-	 * dans l'odre d'apparition dans <code>nonVus</code>
-	 * @param nonVus
-	 * @param sommetCrt
-	 * @param g
-	 */
-	/* public IteratorSeq(Collection<Integer> nonVus, int sommetCrt, Graphe g){
-		this.candidats = new Integer[nonVus.size()];
-		Iterator<Integer> it = nonVus.iterator();
-		while (it.hasNext()){
-			Integer s = it.next();
-			if (g.estArc(sommetCrt, s))
-				candidats[nbCandidats++] = s;
-		}
-	} */
-
 	public IteratorSeq(Collection<Integer> nonVus, int sommetCrt, Graphe g) {
 		
 		GrapheLivraison graphe = (GrapheLivraison) g;
